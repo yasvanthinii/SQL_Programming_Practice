4fa64f77cb9d8f75a84b786360b69d2b203c3a8e
Yasvanthini <yasvanthini.t@gmail.com>
2026-05-13 23:43:58 +0530

    Create insert_values.sql

---
+++ insert_values.sql
@@ -0,0 +1,10 @@
+INSERT INTO Students VALUES
+(1, 'Arun', 'CSE', 85, 'Chennai'),
+(2, 'Priya', 'AI&DS', 92, 'Coimbatore'),
+(3, 'Rahul', 'ECE', 78, 'Madurai'),
+(4, 'Divya', 'IT', 88, 'Salem'),
+(5, 'Kiran', 'CSE', 95, 'Trichy');
+
+INSERT INTO Teachers VALUES
+(101, 'Ramesh', 'DBMS'),
+(102, 'Suresh', 'Python');
