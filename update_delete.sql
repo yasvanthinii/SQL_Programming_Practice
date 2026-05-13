2e7049b8c243a5579900bf07ed25a9d3a53ce72c
Yasvanthini <yasvanthini.t@gmail.com>
2026-05-13 23:46:46 +0530

    Update and delete records in Students table

---
+++ update_delete.sql
@@ -0,0 +1,12 @@
+UPDATE Students
+SET marks = 98
+WHERE student_id = 1;
+
+UPDATE Students
+SET city = 'Bangalore'
+WHERE student_id = 2;
+
+DELETE FROM Students
+WHERE student_id = 3;
+
+SELECT * FROM Students;
