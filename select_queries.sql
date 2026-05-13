d648610ef13d31c91f64c825ab07ad0f118bc73e
Yasvanthini <yasvanthini.t@gmail.com>
2026-05-13 23:44:16 +0530

    Create select_queries.sql

---
+++ select_queries.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Students;
+
+SELECT student_name, marks FROM Students;
+
+SELECT DISTINCT department FROM Students;
+
+SELECT COUNT(*) FROM Students;
+
+SELECT MAX(marks) FROM Students;
+
+SELECT MIN(marks) FROM Students;
+
+SELECT AVG(marks) FROM Students;
