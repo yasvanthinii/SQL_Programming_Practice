a6cad7834e108ac6c089a61485d5d0ed0800a8ad
Yasvanthini <yasvanthini.t@gmail.com>
2026-05-13 23:44:37 +0530

    Create where_clause.sql

---
+++ where_clause.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Students
+WHERE marks > 80;
+
+SELECT * FROM Students
+WHERE department = 'CSE';
+
+SELECT * FROM Students
+WHERE city = 'Chennai';
+
+SELECT * FROM Students
+WHERE marks BETWEEN 80 AND 95;
+
+SELECT * FROM Students
+WHERE student_name LIKE 'A%';
